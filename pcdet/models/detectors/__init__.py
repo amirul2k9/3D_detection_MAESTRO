69d6561054fb38f813cd1b3f05e6107c4dac4105
Izzeddin <ezz.msa@hotmail.com>
2022-09-27 10:39:39 +0100

    update code

--- pcdet/models/detectors/__init__.py
+++ pcdet/models/detectors/__init__.py
@@ -11,7 +11,7 @@ from .centerpoint import CenterPoint
 from .pv_rcnn_plusplus import PVRCNNPlusPlus
 from .mppnet import MPPNet
 from .mppnet_e2e import MPPNetE2E
-from .pillarnet import PillarNet
+# from .pillarnet import PillarNet
 
 __all__ = {
     'Detector3DTemplate': Detector3DTemplate,
@@ -24,7 +24,6 @@ __all__ = {
     'CaDDN': CaDDN,
     'VoxelRCNN': VoxelRCNN,
     'CenterPoint': CenterPoint,
-    'PillarNet': PillarNet,
     'PVRCNNPlusPlus': PVRCNNPlusPlus,
     'MPPNet': MPPNet,
     'MPPNetE2E': MPPNetE2E
